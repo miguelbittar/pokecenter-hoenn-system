d97d3bbc07866ec5842c3129a83154e0869cee41
Miguel Bittar <miguelbittar29@gmail.com>
2025-09-17 18:37:19 -0300

    feat: initial project setup

---
+++ pokecenter-api/src/test/java/com/hoenn/pokecenter/PokecenterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoenn.pokecenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokecenterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
