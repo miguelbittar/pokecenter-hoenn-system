49bf05445393c01d9a00af72aad0edd96e340c28
Miguel Bittar <miguelbittar29@gmail.com>
2025-09-22 10:28:35 -0300

    feat: setup initial Pokemon League microservice
    - Configure separate database (port 5433)
    - Both microservices running successfully (ports 8080/8081)

---
+++ pokemon-league/src/main/java/com/hoenn/pokemonleague/PokemonLeagueApplication.java
@@ -0,0 +1,13 @@
+package com.hoenn.pokemonleague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonLeagueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonLeagueApplication.class, args);
+	}
+
+}
